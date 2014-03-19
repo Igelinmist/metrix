021d04a2ff315f6bebc0036227a3e47fede82f68
Vasiliy N. Troyan <trovas@inbox.ru>
2014-03-19 11:56:52 +0700

    First commit - empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pover::Application.config.secret_key_base = 'e6a688184d4f265abb55529d072eccd01dfe7b34b4ea37e4cf4ac23759fd871cfa81a5f9d6ae1c2c58e36da4a64ac5c443ef1d68c75cd790b8db1d27ae7ca28a'
